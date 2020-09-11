3d65eb1da67446de08aa29ab9d065d95159159f3
TriCodeIT <bayu165kusuma@gmail.com>
2020-09-11 18:16:25 +0700

    Virtual Environment, Django Project Framework, sqlite migrate, create URL

---
+++ website/honda/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    genre_choices = (
+        ('Matic', 'Matic'),
+        ('Bebek', 'Bebek'),
+        ('Sport', 'Sport'),
+        ('BigBike', 'BigBike'),
+    )
+    product = models.CharField(max_length=50)
+    genre = models.CharField(max_length=25, choices=genre_choices, default='Pop')
+    color = models.CharField(max_length=50)
+    rating = models.PositiveIntegerField(default=1)
+
+    def __unicode__(self):
+        return self.product
